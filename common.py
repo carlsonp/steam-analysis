1e4cbd3225b3ff486f6ef10e4450885c848013db
Patrick Carlson <carlson2442@gmail.com>
2019-08-23 12:13:38 -0600

    add comment

--- common.py
+++ common.py
@@ -1,5 +1,6 @@
 # https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
 def sizeof_fmt(num, suffix='B'):
+    # converts bytes to human-friendly string
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
             return "%3.1f %s%s" % (num, unit, suffix)
