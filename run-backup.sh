e15e9633bdea9084400a2646ae720dcad3482189
Patrick Carlson <carlson2442@gmail.com>
2022-01-21 12:24:09 -0700

    only run rysync backup

--- run-backup.sh
+++ run-backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mongodump --archive=./backups/steam-`date +"%m-%d-%y"`.archive --db steam
+#mongodump --archive=./backups/steam-`date +"%m-%d-%y"`.archive --db steam
 rsync -av --delete /home/carlsonp/src/steam-analysis/ /backup2/steam-analysis/
